dqn debug pass; fix random wrong result

DQN.py
@@ -13,7 +13,6 @@ import numpy as np
 import tensorflow as tf
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'   
-#from dqnNode import dqnNode
 # to avoid the warning Your CPU supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA
 
 np.random.seed(1)
@@ -24,7 +23,6 @@ tf.set_random_seed(1)
 class DeepQNetwork:
     def __init__(
             self,
-            dqnNode,
             n_actions,
             n_features,
             learning_rate=0.01,
@@ -34,9 +32,8 @@ class DeepQNetwork:
             memory_size=500,
             batch_size=32,
             e_greedy_increment=None,
-            output_graph=False
-                    
-    ):    # allow dqnNode to call in its attribute
+            output_graph=False,
+    ):
         self.n_actions = n_actions
         self.n_features = n_features
         self.lr = learning_rate
@@ -82,28 +79,28 @@ class DeepQNetwork:
         w_initializer, b_initializer = tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)
 
         # ------------------ build evaluate_net ------------------
-        with tf.variable_scope('eval_net', reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('eval_net'):
             e1 = tf.layers.dense(self.s, 20, tf.nn.relu, kernel_initializer=w_initializer,
                                  bias_initializer=b_initializer, name='e1')
             self.q_eval = tf.layers.dense(e1, self.n_actions, kernel_initializer=w_initializer,
                                           bias_initializer=b_initializer, name='q')
 
         # ------------------ build target_net ------------------
-        with tf.variable_scope('target_net',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('target_net'):
             t1 = tf.layers.dense(self.s_, 20, tf.nn.relu, kernel_initializer=w_initializer,
                                  bias_initializer=b_initializer, name='t1')
             self.q_next = tf.layers.dense(t1, self.n_actions, kernel_initializer=w_initializer,
                                           bias_initializer=b_initializer, name='t2')
 
-        with tf.variable_scope('q_target',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('q_target'):
             q_target = self.r + self.gamma * tf.reduce_max(self.q_next, axis=1, name='Qmax_s_')    # shape=(None, )
             self.q_target = tf.stop_gradient(q_target)
-        with tf.variable_scope('q_eval',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('q_eval'):
             a_indices = tf.stack([tf.range(tf.shape(self.a)[0], dtype=tf.int32), self.a], axis=1)
             self.q_eval_wrt_a = tf.gather_nd(params=self.q_eval, indices=a_indices)    # shape=(None, )
-        with tf.variable_scope('loss',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('loss'):
             self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval_wrt_a, name='TD_error'))
-        with tf.variable_scope('train',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope('train'):
             self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
 
     def store_transition(self, s, a, r, s_):
@@ -163,5 +160,4 @@ class DeepQNetwork:
         plt.show()
 
 if __name__ == '__main__':
-    DQN = DeepQNetwork(dqnNode,3,4, output_graph=True)
-    "order matters"
+    DQN = DeepQNetwork(3,4, output_graph=True)

dqnNode.py
@@ -17,7 +17,7 @@ from dqn import dqn
 
 
 class dqnNode(radioNode):
-    goodChans     = [ ]    
+    goodChans     = [ ]    #
     numStates     = [ ]
     states        = [ ]
     stateHist     = [ ]
@@ -52,7 +52,8 @@ class dqnNode(radioNode):
         self.actions = np.zeros((numChans+1,numChans))
         for k in range(0,numChans):
             self.actions[k+1,k] = 1
-            #for n in range(0,numNodes):        
+            #for n in range(0,numNodes): 
+        self.numChans      = numChans
         self.numActions    = np.shape(self.actions)[0]
         self.actionTally   = np.zeros(numChans+1)
         self.actionHist    = np.zeros((numSteps,numChans))
@@ -76,40 +77,100 @@ class dqnNode(radioNode):
         
         self.policy = np.zeros(numChans)
         
-        self.n_features = 2**numChans   # ToDo
         
-        self.dqn_ = dqn.DeepQNetwork(self.actions, self.n_features,
-                      learning_rate=0.01,
-                      reward_decay=0.9,
-                      e_greedy=0.9,
-                      replace_target_iter=200,
-                      memory_size=2000,
-                      # output_graph=True
-                      )      # nest class ? - YES    
+        
+        self.n_actions     = numChans   # !!! notice
+        self.n_features    = numChans  # TODO  # extreme high later
+        # set real deadline, good to start early
+        # head for GLOBECOMM
+        # Let it be
+        
+        self.dqn_ = dqn(self,self.n_actions, self.n_features,   learning_rate=0.01,reward_decay=0.9,e_greedy=0.9,replace_target_iter=200,memory_size=2000,)      # nest class ? - YES    
+                     # add self - duplicate value
+                     # emit , else type error
 
           
         
         
         
-    def getAction(self,stepNum):
-        return self.dqn_.choose_action(self.rl, self.rl.observation)
+    def getAction(self, stepNum ,observation):
+        temp = self.dqn_.choose_action(observation)  # damn
+        # !!! new define, convert action from a int to a array
+        action       = np.zeros(self.numChans)  # attention, allow WAIT
+        action[temp] = 1 
+        # garbage code - actionHist, actionTally, actionHistInd
+        self.actionHist[stepNum,:] = action
+                    
+        if not np.sum(action):
+            self.actionTally[0] +=    1
+            self.actionHistInd[stepNum] = 1
+        else:
+            self.actionHistInd[stepNum] = np.where(action == 1)[0] + 1
+            self.actionTally[1:] += action
+        return action, temp  # return two type
     
     
-    def getUpdate(self,stepNum, action):
-        #TODO
+    def getReward(self,collision,stepNum):
         
-        # encapsulted together 
-        self.dqn_.store_transition(observation, action, reward, observation_)  
-        #return observation_, reward, done
-        if (stepNum > 200) and (stepNum % 5 == 0): 
-            self.dqn_.learn()
-            
-        observation = observation_
-        if done:
-            break
+        action = self.actionHist[stepNum,:]
+        if not np.sum(action):
+            reward = self.rewards[0]
+            #  rewards = [-200, 100, -100, 50, -200] 
+            self.rewardTally[0] +=  reward
+        else:
+            if not any(np.array(self.goodChans+action) > 1):    # find the 1st of ...
+                if collision == 1:
+                    reward = self.rewards[4]
+                else:
+                    reward = self.rewards[3]               
+            else:
+                if collision == 1:
+                    reward = self.rewards[2]
+                else:
+                    reward = self.rewards[1]                                                         
+            self.rewardTally[1:] += action * reward        
+        self.rewardHist[stepNum] = reward   
         
+        if stepNum == 0:
+            self.cumulativeReward[stepNum] = reward
+        else:
+            self.cumulativeReward[stepNum] = self.cumulativeReward[stepNum-1] + reward
+        return reward   # for debug
+    
+    
+    def storeTransition(self, s, a, r, s_):
+        self.dqn_.store_transition(s, a, r, s_)   # silly, double check after copy-paste
         
-        return reward
+    def learn(self):
+        self.dqn_.learn()
+        
+    
+    
+    
+    
+    
+    
+#    def getUpdate(self,collision,stepNum):
+#        #TODO
+#        
+#        
+#        
+#        
+#        
+#        # encapsulted together 
+#        self.dqn_.store_transition(observation, action, reward, observation_)  
+#        #return observation_, reward, done
+#        if (stepNum > 200) and (stepNum % 5 == 0): 
+#            self.dqn_.learn()
+#            
+#        observation = observation_
+#        if done:
+#            break
+#        
+#        
+#        #TODO - update rewardTally cumulativeReward 
+#        
+#        return reward   # for plot
         
         # notice, return a tuple
         

multiNodeLearning2.py
@@ -16,6 +16,7 @@ from legacyNode  import legacyNode
 from mdpNode     import mdpNode
 from hoppingNode import hoppingNode
 from dqnNode     import dqnNode   #
+from dsaNode     import dsaNode 
 
 from scenario    import scenario
 import matplotlib.pyplot as plt
@@ -30,9 +31,9 @@ tic()
 
 
 # Simulation Parameters
-numSteps = 30000/3   # easier one
+numSteps = 30000   # easier one
 numChans = 4
-nodeTypes = np.array( [5,3])    #
+nodeTypes = np.array( [5,2,2,0])    #
 # The type of each node 
 #0 - Legacy (Dumb) Node 
 #1 - Hopping Node
@@ -59,7 +60,7 @@ t = mdpNode(numChans,states,numSteps)     # breakpoint
 
 for k in range(0,numNodes):
     if nodeTypes[k] == 0:
-        t = legacyNode(numChans,numSteps,legacyTxProb)
+        t = legacyNode(numChans,numSteps,legacyTxProb,0)  # modify
 
     elif nodeTypes[k] == 1:
         t = hoppingNode(numChans,numSteps)
@@ -81,7 +82,7 @@ for k in range(0,numNodes):
         
 nodes[0].goodChans = np.array( [1,1,0,0] )        
 nodes[1].goodChans = np.array( [0,1,1,0] )          
-nodes[2].goodChans = np.array( [0,0,1,1] )    
+#nodes[2].goodChans = np.array( [0,0,1,1] )    
 
 simulationScenario = scenario(numSteps,'fixed',3)  
 
@@ -99,9 +100,9 @@ print "Starting Main Loop"
 
 
 ###############################   DEBUG SENTENCE #############################                
-action = nodes[0].getAction(0)
+#action = nodes[0].getAction(0,observation)
 
-reward = nodes[0].getReward(collisions[0],0)
+#reward = nodes[0].getReward(collisions[0],0)
 
 
 
@@ -128,7 +129,11 @@ for s in range(0,numSteps):
     
     #Determination of next action for each node
     for n in range(0,numNodes):
-        actions[n,:] = nodes[n].getAction(s)
+        if isinstance(nodes[n],dqnNode):
+            observation = observedStates[n,:]
+            actions[n,:], actionScalar = nodes[n].getAction(s, observation)  ###########
+        else:    
+            actions[n,:] = nodes[n].getAction(s)
         assert not any(np.isnan(actions[n,:])), "ERROR! action is Nan"
         # good for quick guess than step by step
         
@@ -162,13 +167,21 @@ for s in range(0,numSteps):
                 
 ###############################   DQN BEGIN #############################                
         if isinstance(nodes[n],dqnNode):
-            reward = nodes[n].getUpdate(collisions[n],s)
+            reward = nodes[n].getReward(collisions[n],s)
+            observation_ = observedStates[n,:]  # update already
+            done = True
+            nodes[n].storeTransition(observation, actionScalar, reward, observation_)
+            if s > 200 and s % 5 == 0:
+                nodes[n].learn()
+
+            # original  action -> step() -> observation_, reward, done
+            # 1. action -> collisions
+            # 2. collisions -> getReward() -> observation_, reward, done
+
+            
             #TODO the env - RL take action and get next observation and reward
              # observation_, reward, done = env.step(action)
              # 1. step - get s_ r; 2. store 3. learn
-            
-            
-
 ###############################   DQN END #############################                
 
                   
@@ -185,78 +198,137 @@ toc()
 
 
                  
-#########################################################################
-##############################plot session###############################
-#
-#txPackets = [ ]
-#
-#
-################ cumulativeCollisions ##################
-#plt.figure(1)
-##plt.hold()
-#legendInfo = [ ]
-#for n in range(0,numNodes):
-#    plt.plot(cumulativeCollisions[:,n])      # <<<<
-##    if isinstance(nodes[n],dsaNode):
-##        legendInfo = 'Node %d (DSA)'%{n}
-#    if isinstance(nodes[n],hoppingNode):
-#        legendInfo.append( 'Node %d (Hopping)'%(n) )
-#    elif isinstance(nodes[n],mdpNode):
-#        legendInfo.append( 'Node %d (MDP)'%(n) )
-#    else:
-#        legendInfo.append( 'Node %d (Legacy)'%(n) )
-#    
-#    txPackets.append( np.cumsum(np.sum(nodes[n].actionHist.T).T ) )
-#plt.legend(legendInfo)
-#plt.xlabel('Step Number')
-#plt.ylabel('Cumulative Collisions')
-#plt.title( 'Cumulative Collisions Per Node')                      
-#plt.show()
-#            
-#        
-################ cumulativeReward ##################
-#plt.figure(2)
-##plt.hold()  #deprecate
-#c = 1
-#legendInfo = [ ]
-#for n in range(0,numNodes):
-#    if isinstance(nodes[n],mdpNode):
-#        plt.plot(nodes[n].cumulativeReward)
-#        legendInfo.append('Node %d (MDP)'%(n) )
-#if legendInfo:
-#    plt.legend(legendInfo)
-#    plt.xlabel('Step Number')
-#    plt.ylabel('Cumulative Reward')
-#    plt.title( 'Cumulative Reward Per Node')   
-#plt.show()             
-#        
-##np.ceil
-################ Actions #################################
-#plt.figure(3)
-#split = np.ceil(numNodes / 2)    
-#for n in range(0,numNodes):
-#    if n <= split:
-#        plt.subplot(split,2,n+1)
-#    else:
-#        plt.subplot(split,2,n+1)
-#        
-#    if isinstance(nodes,mdpNode):
-#        offset = 1
-#    else:
-#        offset = 0
-#    plt.plot( np.maximum(nodes[n].actionHistInd-1 , np.zeros(numSteps)),'bo' )
-#    plt.ylim(0,numChans+2)
-#    plt.xlabel('Step Number')
-#    plt.ylabel('Action Number')
-#    
-#    if isinstance(nodes[n],legacyNode):
-#        titleLabel = 'Action Taken by Node %d (Legacy)'%(n)
-#    elif isinstance(nodes[n],hoppingNode):
-#        titleLabel = 'Action Taken by Node %d (Hopping)'%(n)
-#    else:
-#        titleLabel = 'Action Taken by Node %d (MDP)'%(n)   # no dsa
-#    plt.title(titleLabel)
+########################################################################
+#############################plot session###############################
 
+txPackets = [ ]
+
+
+############### 1 cumulativeCollisions ##################
+plt.figure(1)
+#plt.hold()
+legendInfo = [ ]
+for n in range(0,numNodes):
+    plt.plot(cumulativeCollisions[:,n])      # <<<<
+#    if isinstance(nodes[n],dsaNode):
+#        legendInfo = 'Node %d (DSA)'%{n}
+    if isinstance(nodes[n],hoppingNode):
+        legendInfo.append( 'Node %d (Hopping)'%(n) )
+    elif isinstance(nodes[n],mdpNode):
+        legendInfo.append( 'Node %d (MDP)'%(n) )
+    elif isinstance(nodes[n],dsaNode):
+        legendInfo.append( 'Node %d (DSA)'%(n) )
+    elif isinstance(nodes[n],dqnNode):
+        legendInfo.append( 'Node %d (DQN)'%(n) )
+    else:
+        legendInfo.append( 'Node %d (Legacy)'%(n) )
+    
+    txPackets.append( np.cumsum(np.sum(nodes[n].actionHist.T , axis=0).T ) )
+plt.legend(legendInfo)
+plt.xlabel('Step Number')
+plt.ylabel('Cumulative Collisions')
+plt.title( 'Cumulative Collisions Per Node')                      
+plt.show()
+            
+        
+############### 2 cumulativeReward ##################
+plt.figure(2)
+#plt.hold()  #deprecate
+#c = 1
+legendInfo = [ ]
+for n in range(0,numNodes):
+    if isinstance(nodes[n],mdpNode):
+        plt.plot(nodes[n].cumulativeReward)
+        legendInfo.append('Node %d (MDP)'%(n) )
+    elif isinstance(nodes[n],dqnNode):
+        plt.plot(nodes[n].cumulativeReward)
+        legendInfo.append('Node %d (DQN)'%(n) )
+if legendInfo:
+    plt.legend(legendInfo)
+    plt.xlabel('Step Number')
+    plt.ylabel('Cumulative Reward')
+    plt.title( 'Cumulative Reward Per Node')   
+plt.show()             
+        
+#np.ceil
+############### 3 Actions #################################
+plt.figure(3)
+split = np.ceil(numNodes / 2)    
+for n in range(0,numNodes):
+    if n <= split:
+        plt.subplot(split,2,n+1)
+    else:
+        plt.subplot(split,2,n+1)
+        
+    if isinstance(nodes,mdpNode):
+        offset = 1
+    else:
+        offset = 0
+    plt.plot( np.maximum(nodes[n].actionHistInd-1 , np.zeros(numSteps)),'bo' )
+    plt.ylim(0,numChans+2)
+    plt.xlabel('Step Number')
+    plt.ylabel('Action Number')
+    
+    if   isinstance(nodes[n],legacyNode):
+        titleLabel = 'Action Taken by Node %d (Legacy)'%(n)
+    elif isinstance(nodes[n],hoppingNode):
+        titleLabel = 'Action Taken by Node %d (Hopping)'%(n)
+    elif isinstance(nodes[n],dsaNode):
+        titleLabel = 'Action Taken by Node %d (DSA)'%(n)
+    elif isinstance(nodes[n],mdpNode):
+        titleLabel = 'Action Taken by Node %d (MDP)'%(n)
+    else:
+        titleLabel = 'Action Taken by Node %d (DQN)'%(n)   # no dsa
+    plt.title(titleLabel)
+
+    
+
+############### 4 Packet Error Rate  #################################
+timeSlots = np.matlib.repmat( np.arange(1,numSteps+1)[np.newaxis].T  ,1,numNodes )  
+txPackets = np.array(txPackets).T
+PER =  cumulativeCollisions / txPackets
+PLR = (cumulativeCollisions + timeSlots - txPackets) /timeSlots
+    
+plt.figure(4)
+legendInfo = [ ]
+for i in range(numNodes):
+    if isinstance(nodes[i],mdpNode):
+        plt.semilogy( PER[:,i] )
+        legendInfo.append( 'Node %d (MDP)'%(i) )
+    elif isinstance(nodes[i],dsaNode):
+        plt.semilogy( PER[:,i] )
+        legendInfo.append( 'Node %d (DSA)'%(i) )
+    elif isinstance(nodes[i],dqnNode):
+        plt.semilogy( PER[:,i] )
+        legendInfo.append( 'Node %d (DQN)'%(i) )
+    else:
+        plt.semilogy( PER[:,i] )
+        legendInfo.append( 'Node %d (legacy)'%(i) )
+plt.legend(legendInfo)
+plt.xlabel('Step Number')
+plt.ylabel('Cumulative Packet Error Rate')
+plt.title( 'Cumulative Packet Error Rate')                      
+plt.show()        
+        
+
+############### 5 Packet Loss Rate  #################################
+plt.figure(5)
+legendInfo = [ ]
+for i in range(numNodes):
+    if isinstance(nodes[i],mdpNode):
+        plt.semilogy( PLR[:,i] )
+        legendInfo.append( 'Node %d (MDP)'%(i) )
+    elif isinstance(nodes[i],dqnNode):
+        plt.semilogy( PLR[:,i] )
+        legendInfo.append( 'Node %d (DQN)'%(i) )
+if not legendInfo:
+    plt.xlabel('Step Number')
+    plt.ylabel('Cumulative Packet Loss Rate')
+    plt.title( 'Cumulative Packet Loss Rate')                      
+    plt.show() 
+    
+
+############### END OF PLOT  ################################# 
     
     
     